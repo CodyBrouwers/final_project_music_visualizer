e2f78ebae30d43044218e46b382b28f0d7c91acc
Bruno Opsenica <bruno.opsenica@gmail.com>
2015-06-18 15:43:34 -0400

    Fix the menu

--- public/javascript/Menu.js
+++ public/javascript/Menu.js
@@ -19,6 +19,7 @@ var Menu = function() {
     e.style.transform = 'translateX(' + v + 'px)';
   }
 
+  //KEEP THIS IF YOU START OVER
   $('.color').on('blur', 'li', function(event) {
     var red = $('#input-red').val() + ", ";
     var green = $('#input-green').val() + ", ";
@@ -36,7 +37,8 @@ var Menu = function() {
       mesh.geometry = new THREE.BoxGeometry( 200, 200, 200 );
     }
   });
-
+  //-----------
+  //
   btn.addEventListener('click', function() {
     btn.style.opacity = 0;
     menu.style.opacity = 1;
