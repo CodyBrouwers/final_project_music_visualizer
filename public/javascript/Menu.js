c39651b99c0d2099a3d186e19231168f94b1763e
andrewheppner <andrewbheppner@gmail.com>
2015-06-18 21:23:25 +0000

    cleaned up unused code on menu.js

--- public/javascript/Menu.js
+++ public/javascript/Menu.js
@@ -1,19 +1,10 @@
 var Menu = function() {
 
-  var width = 240;
-
-  var effectCallback, micCallback, trackCallback;
-
   var menu = document.querySelector(".menu");
   var showbtn = document.querySelector(".menu-button");
   var hidebtn = document.querySelector(".close");
-  var effects = document.querySelectorAll(".colors li");
+  
 
-  // var mic = document.querySelector(".mic");
-  var track = document.querySelector(".track");
-
-
-  //KEEP THIS IF YOU START OVER
   $('.color').on('blur', 'li', function(event) {
     var red = $('#input-red').val() + ", ";
     var green = $('#input-green').val() + ", ";
@@ -45,33 +36,6 @@ var Menu = function() {
     hidebtn.style.display = 'none';
     
   });
-
-  track.addEventListener('click', function() {
-    mic.setAttribute('class', '');
-    track.setAttribute('class', 'selected');
-    if(trackCallback) trackCallback();
-  });
-
-  // var m = {};
-
-  // m.onEffect = function(callback) {
-  //   effectCallback = callback;
-  // }
-
-  // m.onMic = function(callback) {  
-  //   micCallback = callback;
-    
-  // }
-
-  // m.onTrack = function(callback) {
-  //   trackCallback = callback;
-  // }
-
-  // track.setAttribute('class', 'selected');
-  // btn.style.opacity = 0;
-  // btn.style.display = 'none';
-
-  // return m;
 
 };
 
