c2d8d0c65a36de7328914778d4d8542fb1b1fb2f
andrewheppner <andrewbheppner@gmail.com>
2015-06-23 20:58:48 -0400

    Application.jsx file should be broken up into seperate component files to make navigating the project easier

---
+++ public/javascript/components/audiowave.jsx
@@ -0,0 +1,65 @@
+var musicInterface = Object.create(MusicInterface);
+
+var AudioWave = React.createClass({
+    //Playback
+    backward: function() {
+      musicInterface.skipBackward();
+    },
+    forward: function() {
+      musicInterface.skipForward();
+    },
+    playPause: function() {;
+      musicInterface.playPause();
+    },
+    toggleMute: function() {
+      musicInterface.toggleMute();
+    },
+
+    addTransition: function() {
+      var time = musicInterface.getCurrentTime();
+      this.props.postTransition(this.props.visualization.id, time, musicInterface.getVisualizerParams());
+    },
+
+    render: function(){
+      return (
+        <div className="wave-container">
+          <div className="controls">
+            <button onClick={this.backward}>Backwards</button>
+            <button onClick={this.playPause}>Play/Pause</button>
+            <button onClick={this.forward}>Forwards</button>
+            <button onClick={this.toggleMute}>Mute</button>
+            <button onClick={this.addTransition}>Add Transition</button>
+          </div>
+          <div id="wave"></div>
+          <div id="wave-timeline"></div>
+
+          <p id="drop">Drop your file here</p>
+        </div>
+      );
+    },
+    componentDidMount: function () {
+      musicInterface.init();
+
+      // Initializes timeline plugin and plays once ready
+      musicInterface.waveSurfer.on('ready', function () {
+        var timeline = Object.create(WaveSurfer.Timeline);
+
+        timeline.init({
+          wavesurfer: musicInterface.waveSurfer,
+          container: "#wave-timeline"
+        });
+
+        musicInterface.waveSurfer.on('region-click', function (region, e) {
+          // Play on click, loop on shift click
+          e.shiftKey ? region.playLoop() : region.play();
+        });
+
+        musicInterface.waveSurfer.on('region-dblclick', function (region, e) {
+          region.remove();
+        });
+
+        // TODO - Load JSON data in when ready
+        // loadRegions(JSON.parse(localStorage.transitions));
+      });
+    }
+  })
